faf09f4995bb899d78a166ed99c8dfc65d2f456d
Joan Gerard <joan.gerard@outlook.com>
2022-06-08 17:48:47 +0200

    update README

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## The app
 
-Customer List
+Customer List App:
 
 * The user is able to view a list of customers on the Home screen
 * The user is able to click on a name and navigate to another screen to view more details about the selected customer
@@ -14,3 +14,17 @@ Customer List
 ## The data
 
 There is a file that contains a 20 random customers (generated from [RandomUser](https://randomuser.me/)).
+
+## Getting Started
+
+Clone this repositery and install the dependencies:
+
+### `npm install`
+
+Once the dependencies are installed, run the server:
+
+### `npm start`
+
+Open [http://localhost:19002](http://localhost:19002) to view de developer tool in the browser.
+
+On the developer tool you can choose to run the app on iOS or Android simulator, or in the web browser
--- components/Home.js
+++ components/Home.js
@@ -1,29 +1,32 @@
-import { Text, FlatList, View, Button } from 'react-native';
-import { useState } from 'react';
-import customer from '../customer/customer.js'
+import { Text, FlatList, View, Button } from "react-native";
+import { useState } from "react";
+import customer from "../customer/customer.js";
 
 const Home = ({ navigation }) => {
+  const [customerState, setcustomeState] = useState(customer);
 
-    const [customerState, setcustomeState] = useState(customer)
-
-    console.log(customerState.customers[0])
-
-    return (
-        <>
-            <Text>List of Customers</Text>
+  return (
+    <>
+      <Text>List of Customers</Text>
+      <View>
+        <FlatList
+          keyExtractor={(item) => item.cell}
+          data={customerState.customers}
+          renderItem={({ item }) => (
             <View>
-                <FlatList
-                    keyExtractor={(item) => item.cell}
-                    data={customerState.customers}
-                    renderItem={({ item }) => (
-                        <View>
-                            <Text>{item.name.first}</Text>
-                            <Button onPress={() => navigation.navigate('Profile', { customer: item })} title="See profile" />
-                        </View>
-                    )} />
+              <Text>{item.name.first}</Text>
+              <Button
+                onPress={() =>
+                  navigation.navigate("Profile", { customer: item })
+                }
+                title="See profile"
+              />
             </View>
-        </>
-    )
+          )}
+        />
+      </View>
+    </>
+  );
 };
 
 export default Home;
